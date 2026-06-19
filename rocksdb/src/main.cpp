b37008164d450192c7ec6f35760e8ab9ef9fb05c
Valentin Daniel <valentin2daniel3@gmail.com>
2026-06-19 20:10:17 +0000

    styled

--- rocksdb/src/main.cpp
+++ rocksdb/src/main.cpp
@@ -1,16 +1,19 @@
-#include "approcksdb/cli.hpp"
-#include "approcksdb/feature_runner.hpp"
+#include <cstdio>
+#include <unistd.h>
+#include <errno.h>
+#include <uk/print.h>
+
+extern "C" int rocksdb_test_main(void);
 
 int main(int argc, char** argv)
 {
-	approcksdb::AppConfig cfg;
-	int parsed = approcksdb::ParseArgs(argc, argv, &cfg);
-
-	if (parsed > 0)
-		return 0;
-	if (parsed < 0)
-		return 1;
-
-	approcksdb::FeatureRunner runner(cfg);
-	return runner.Run();
+	const char msg[] = "[app] main() entered\n";
+	ssize_t r1 = write(1, msg, sizeof(msg) - 1);
+	ssize_t r2 = write(2, msg, sizeof(msg) - 1);
+	uk_pr_info("[app] write(1)=%zd errno=%d write(2)=%zd errno=%d\n",
+		   r1, errno, r2, errno);
+	uk_pr_info("[app] calling rocksdb_test_main()\n");
+	int r = rocksdb_test_main();
+	uk_pr_info("[app] rocksdb_test_main() returned %d\n", r);
+	return r;
 }
